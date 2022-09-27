e8bc8a0a4d988b7347b7385aa23fd3f46c5d30c5
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-09-27 20:27:24 -0300

    feat: simulating transaction of Executive

--- JavaExercicios/praticaClasseAbstrata/src/bank/App.java
+++ JavaExercicios/praticaClasseAbstrata/src/bank/App.java
@@ -1,4 +1,16 @@
 package bank;
 
+import bank.typeOfUsers.Executive;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("--Executive--");
+        Executive executive = new Executive();
+
+        executive.doDeposit(200.00);
+        executive.transacaoOk("Deposit");
+
+        executive.doTransference(800.00);
+        executive.transacaoNaoOk("Transference");
+    }
 }
