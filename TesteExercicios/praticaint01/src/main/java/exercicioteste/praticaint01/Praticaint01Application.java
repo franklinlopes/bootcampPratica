46606d6a842e606dc518fffabdf7fcfa69dfa75e
franklinlopes <gabriel.franklin@mercadolivre.com>
2022-10-13 12:10:42 -0300

    initial commit

---
+++ TesteExercicios/praticaint01/src/main/java/exercicioteste/praticaint01/Praticaint01Application.java
@@ -0,0 +1,13 @@
+package exercicioteste.praticaint01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praticaint01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praticaint01Application.class, args);
+	}
+
+}
---
+++ TesteExercicios/praticaint01/src/test/java/exercicioteste/praticaint01/Praticaint01ApplicationTests.java
@@ -0,0 +1,13 @@
+package exercicioteste.praticaint01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praticaint01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
